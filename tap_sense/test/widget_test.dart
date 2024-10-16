15d4cb3533d5af507a0bbd50f98cd911f556bcd9
Rajin Khan <rajin.khan2001@gmail.com>
2024-10-16 11:43:42 +0600

    Update folder

--- tap_sense/test/widget_test.dart
+++ tap_sense/test/widget_test.dart
@@ -8,16 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-/* import 'package:tap_sense/main.dart';
-  uncomment this part to make app work
-*/
+import 'package:tap_sense/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    /* await tester.pumpWidget(const MyApp());
-      uncomment this part to make app work
-    */
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
