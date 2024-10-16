904d601066827c6d8b3bfd5a205948684787fdba
Hunter <hunter@Hunters-MacBook-Air.local>
2024-10-16 16:13:10 +0600

    Update screens and map them to buttons

--- tap_sense/lib/sections/menu_screen.dart
+++ tap_sense/lib/sections/menu_screen.dart
@@ -1,88 +1,159 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tap_sense/sections/education/education_screen.dart';
+import 'package:tap_sense/sections/navigation/navigation_screen.dart';
+import 'package:tap_sense/sections/nutrition/nutrition_screen.dart';
+import 'package:tap_sense/sections/productivity/productivity_screen.dart';
 
-class MenuScreen extends StatelessWidget { //remember to convert to stateful widget and add buttons for all four sections
-  const MenuScreen({super.key});
+class MenuScreen extends StatefulWidget {
+  MenuScreen(this.changeScreen, {super.key});
+  final void Function() changeScreen;
+  @override
+  State<MenuScreen> createState() {
+    return _MenuScreenState();
+  }
+}
+
+var num = 0;
+
+class _MenuScreenState extends State<MenuScreen> {
+  var activeScreen = 'menu-screen';
+
+  void switchScreen() {
+    setState(() {
+      if (num == 1) {
+        activeScreen = 'education-screen';
+      } else if (num == 2) {
+        activeScreen = 'productivity-screen';
+      } else if (num == 3) {
+        activeScreen = 'navigation-screen';
+      } else if (num == 4) {
+        activeScreen = 'nutrition-screen';
+      } else {
+        activeScreen = 'menu-screen'; // Default to menu
+      }
+    });
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
+    Widget screenWidget = buildMenu();
+
+    // Determine which screen to show based on activeScreen
+    if (activeScreen == 'education-screen') {
+      screenWidget = const EducationScreen();
+    } else if (activeScreen == 'productivity-screen') {
+      screenWidget = const ProductivityScreen();
+    } else if (activeScreen == 'navigation-screen') {
+      screenWidget = const NavigationScreen();
+    } else if (activeScreen == 'nutrition-screen') {
+      screenWidget = const NutritionScreen();
+    } else {
+      screenWidget = buildMenu(); // Build Menu screen
+    }
+
     return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: screenWidget,
+      ),
+    );
+  }
+
+  // This function builds the Menu layout
+  Widget buildMenu() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'PICK ONE OF THE FOUR CORNERS:',
+          style: GoogleFonts.poppins(
+            color: const Color.fromRGBO(228, 239, 244, 1),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 280),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'PICK ONE OF THE FOUR CORNERS:',
-              style: GoogleFonts.poppins(
-                color: const Color.fromRGBO(228, 239, 244, 1),
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 280),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'EDUCATION',
-                  style: GoogleFonts.poppins(
-                    color: const Color.fromRGBO(228, 239, 244, 1),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Text(
-                  'PRODUCTIVITY',
-                  style: GoogleFonts.poppins(
-                    color: const Color.fromRGBO(228, 239, 244, 1),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'NAVIGATION',
-                  style: GoogleFonts.poppins(
-                    color: const Color.fromRGBO(228, 239, 244, 1),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Text(
-                  'NUTRITION',
-                  style: GoogleFonts.poppins(
-                    color: const Color.fromRGBO(228, 239, 244, 1),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 320),
             TextButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                num = 1;
+                switchScreen();
+              }, // Trigger screen switch on button press
               style: TextButton.styleFrom(
                 foregroundColor: Colors.white,
                 textStyle: GoogleFonts.poppins(
                     fontSize: 12, fontWeight: FontWeight.bold),
               ),
-              icon: const Icon(Icons.multitrack_audio_rounded),
-              label: const Text('READ SCREEN'),
-            )
+              icon: const Icon(Icons.book),
+              label: const Text('EDUCATION'),
+            ),
+            TextButton.icon(
+              onPressed: () {
+                num = 2;
+                switchScreen();
+              }, // Trigger screen switch on button press
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: GoogleFonts.poppins(
+                    fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+              icon: const Icon(Icons.work),
+              label: const Text('PRODUCTIVITY'),
+            ),
           ],
         ),
-      ),
+        const SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () {
+                num = 3;
+                switchScreen();
+              }, // Trigger screen switch on button press
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: GoogleFonts.poppins(
+                    fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+              icon: const Icon(Icons.navigation),
+              label: const Text('NAVIGATION'),
+            ),
+            TextButton.icon(
+              onPressed: () {
+                num = 4;
+                switchScreen();
+              }, // Trigger screen switch on button press
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: GoogleFonts.poppins(
+                    fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+              icon: const Icon(Icons.food_bank),
+              label: const Text('NUTRITION'),
+            ),
+          ],
+        ),
+        const SizedBox(height: 320),
+        TextButton.icon(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle:
+                GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.bold),
+          ),
+          icon: const Icon(Icons.multitrack_audio_rounded),
+          label: const Text('READ SCREEN'),
+        )
+      ],
     );
   }
 }
--- tap_sense/lib/sections/productivity/productivity_screen.dart
+++ tap_sense/lib/sections/productivity/productivity_screen.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProductivityScreen extends StatelessWidget {
   const ProductivityScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Text('Productivity Screen');
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 0, 0, 0),
+              Color.fromARGB(255, 60, 40, 3),
+              Color.fromARGB(255, 72, 1, 33),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'PRODUCTIVITY SCREEN',
+            style: GoogleFonts.poppins(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
