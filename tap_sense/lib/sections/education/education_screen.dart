e4e09261579cb007de83ed5f9aa6231a54968921
Rajin Khan <rajin.khan2001@gmail.com>
2024-10-24 18:36:03 +0600

    Restore back to previous working version

--- tap_sense/lib/sections/education/education_screen.dart
+++ tap_sense/lib/sections/education/education_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class EducationScreen extends StatelessWidget {
+class EducationScreen extends StatefulWidget {
   const EducationScreen(this.onRestart, {super.key});
   final void Function() onRestart;
 
+  @override
+  State<EducationScreen> createState() => _EducationScreenState();
+}
+
+class _EducationScreenState extends State<EducationScreen> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,7 +31,7 @@ class EducationScreen extends StatelessWidget {
 
         const SizedBox(height: 20),
         TextButton(
-          onPressed: onRestart,
+          onPressed: widget.onRestart,
           style: TextButton.styleFrom(
             foregroundColor: Colors.white,
               textStyle: GoogleFonts.poppins(
