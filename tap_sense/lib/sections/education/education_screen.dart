20bc06b92ab6991b9ce8e3411013225395b5beed
Rajin Khan <rajin.khan2001@gmail.com>
2024-10-27 16:19:36 +0600

    Add OCR code

--- tap_sense/lib/sections/education/education_screen.dart
+++ tap_sense/lib/sections/education/education_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EducationScreen extends StatefulWidget {
   const EducationScreen(this.onRestart, {super.key});
@@ -10,6 +14,39 @@ class EducationScreen extends StatefulWidget {
 }
 
 class _EducationScreenState extends State<EducationScreen> {
+  File? _imageFile; //creates the image file
+  String? extractedText;
+
+  Future<void> _pickImage() async {
+    //image picker
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+      _processImage();
+    }
+  }
+
+  Future<void> _processImage() async {
+    //OCR processor
+    final inputImage = InputImage.fromFilePath(_imageFile!.path);
+    final textRecognizer = TextRecognizer();
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+    extractedText = recognizedText.text;
+    print(extractedText); //print to terminal (execute to separate screen later)
+    //return extractedText
+  }
+
+  /*String? processedOutput() {
+    setState(() {
+      extractedText == null ? 'Pick image' : extractedText!;
+    });
+    return extractedText;
+  }*/
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,18 +63,23 @@ class _EducationScreenState extends State<EducationScreen> {
           textAlign: TextAlign.center,
         ),
         const SizedBox(height: 20),
-
-        //enter image picker widget here
-
+        TextButton(
+          onPressed: _pickImage,
+          child: const Icon(
+            Icons.add_a_photo_outlined,
+            color: Colors.white,
+          ),
+        ),
+        //Text(processedOutput()),
         const SizedBox(height: 20),
         TextButton(
           onPressed: widget.onRestart,
           style: TextButton.styleFrom(
             foregroundColor: Colors.white,
-              textStyle: GoogleFonts.poppins(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+            textStyle: GoogleFonts.poppins(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           child: const Text('BACK'),
         ),
