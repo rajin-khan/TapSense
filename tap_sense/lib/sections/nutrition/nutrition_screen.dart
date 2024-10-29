d6366c3a9f47ffccce014d04659871af169d42f3
Rajin Khan <rajin.khan2001@gmail.com>
2024-10-29 12:32:13 +0600

    Update to stateful widget, add base model object detection capability

--- tap_sense/lib/sections/nutrition/nutrition_screen.dart
+++ tap_sense/lib/sections/nutrition/nutrition_screen.dart
@@ -1,9 +1,59 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
 
-class NutritionScreen extends StatelessWidget {
+import 'package:image_picker/image_picker.dart';
+
+class NutritionScreen extends StatefulWidget {
   const NutritionScreen(this.onRestart, {super.key});
   final void Function() onRestart;
+
+  @override
+  State<NutritionScreen> createState() => _NutritionScreenState();
+}
+
+class _NutritionScreenState extends State<NutritionScreen> {
+  File? _imageFile;
+
+  Future<void> _pickImage() async {
+    //image picker
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+      _processImage();
+    }
+  }
+
+  Future<void> _processImage() async {
+    final inputImage = InputImage.fromFilePath(_imageFile!.path);
+
+    final objectDetector = ObjectDetector(
+      //configure object detector
+      options: ObjectDetectorOptions(
+        mode: DetectionMode.single,
+        classifyObjects: true,
+        multipleObjects: false,
+      ),
+    );
+
+    final List<DetectedObject> objects =
+        await objectDetector.processImage(inputImage);
+
+    for (DetectedObject detectedObject in objects) {
+      //final rect = detectedObject.boundingBox;
+      //final trackingId = detectedObject.trackingId;
+
+      for (Label label in detectedObject.labels) {
+        print('${label.text} ${label.confidence}');
+      }
+    }
+
+    objectDetector.close();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +70,24 @@ class NutritionScreen extends StatelessWidget {
           ),
           textAlign: TextAlign.center,
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 18),
+        TextButton.icon(
+          //button for image picking
+          onPressed: _pickImage,
+          label: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 4),
         TextButton(
-          onPressed: onRestart,
+          onPressed: widget.onRestart,
           style: TextButton.styleFrom(
             foregroundColor: Colors.white,
-              textStyle: GoogleFonts.poppins(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+            textStyle: GoogleFonts.poppins(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           child: const Text('BACK'),
         ),
